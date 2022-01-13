d38cdadc6776df1ce80ce4cbeeddf4c60566cc9e
Seyan Singampalii <84675733+seyan-s@users.noreply.github.com>
2022-01-14 00:00:39 +0530

    Add files via upload
    this is my work! i have made a random password generator

---
+++ Random_Password.py
@@ -0,0 +1,14 @@
+import random
+def randpass():
+
+     chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ@!$#()*&1234567890"
+
+     password = ""
+     while True:
+          password_len = int(input("what is the length of your password:"))
+          for x in range(0,password_len):
+               password_chars = random.choice(chars)
+               password = password + password_chars
+          return password
+
+print(randpass())          
