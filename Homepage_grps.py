d7d7f87d76016f5167674041dd8a883d7fa25b31
KanishTech <kanish08.tech@gmail.com>
2022-01-16 00:01:56 +0530

    minor change

--- Homepage_grps.py
+++ Homepage_grps.py
@@ -8,27 +8,6 @@ import string
 
 
 def add():
-    '''def generate_password():
-        letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
-                'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
-                'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-        numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-        symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
-
-        nr_letters = random.randint(8, 10)
-        nr_symbols = random.randint(2, 4)
-        nr_numbers = random.randint(2, 4)
-
-        password_letters = [random.choice(letters) for _ in range(nr_letters)]
-        password_symbols = [random.choice(symbols) for _ in range(nr_symbols)]
-        password_numbers = [random.choice(numbers) for _ in range(nr_numbers)]
-
-        password_list = password_letters + password_numbers + password_symbols
-        random.shuffle(password_list)
-        password = "".join(password_list)
-
-        pas_entry.insert(0, password)
-        pyperclip.copy(password)'''
     def randpass():
         length = 10
         small = string.ascii_lowercase 
@@ -76,7 +55,7 @@ def add():
     web_text.place(x=5,y=225)
     web_entry=tkinter.Entry(wim,width=50,fg="black", bg="#64f586")
     web_entry.place(x=150,y=225)
-    Go_Button=tkinter.Button(wim,text="GO",bg="#00ff95",fg="green",width=25,height=2,activebackground='#64f586')
+    Go_Button=tkinter.Button(wim,text="ADD",bg="#00ff95",fg="green",width=25,height=2,activebackground='#64f586')
     Go_Button.place(x=150,y=275)
     
     
