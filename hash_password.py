4135ad0738074dc854386e28f09accbcedcf2143
KanishTech <kanish08.tech@gmail.com>
2022-01-17 00:30:43 +0530

    Delete hash_password.py

--- hash_password.py
+++
@@ -1,9 +0,0 @@
-import hashlib
-
-password="password"
-def hashPassword(input):
-    hash=hashlib.md5(input)
-    hash = hash.hexdigest()
-    return hash
-print(hashPassword(password.encode('utf=8')))
-
