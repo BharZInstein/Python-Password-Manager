5f19134b98b29f05b4b6c0900ead351af56fe33b
KanishTech <kanish08.tech@gmail.com>
2022-01-16 19:34:49 +0530

    hashing password

---
+++ hashing.py
@@ -0,0 +1,12 @@
+import hashlib
+def make_pw_hash(password):
+    password=password.encode('utf-8')
+    hash1= hashlib.md5(password)
+    hash1= hash1.hexdigest()
+    return hash1
+def check_pw_hash(password, hash2):
+    if make_pw_hash(password)== hash2:
+        return True
+    else:
+        return False
+
