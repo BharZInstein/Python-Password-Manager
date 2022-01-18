f2d2e84e84c90439b4f77836922cee73ab7a586b
Adhiraj Singh <75022578+adhirajsingh1806@users.noreply.github.com>
2022-01-18 23:35:24 +0530

    Update BharZ_CscProject_GUI.py

--- BharZ_CscProject_GUI.py
+++ BharZ_CscProject_GUI.py
@@ -1,6 +1,7 @@
 #GUI FOR CSC PROJECT by Bharghav
 import tkinter
 import tkinter.font
+import sqlite3
 win=tkinter.Tk()
 win.title("The Bois Password Manager")
 win.geometry("920x640")
@@ -28,12 +29,31 @@ entry2=tkinter.Entry(show="*",fg="black", bg="#64f586", width=50)
 entry2.place(x=180,y=120)
 Username=entry1.get()
 M_Password=entry2.get()
+
+def login():
+    conn = sqlite3.connect('pass_manager.db')
+    c = conn.cursor()
+    
+   
+    c.execute("INSERT INTO user_data VALUES(:username, :master_pwd)",
+            {
+                'username': Username,
+                'master_pwd': M_Password,
+            })
+
+    conn.commit() 
+    conn.close()
+
+    entry1.delete(0,"end")
+    entry2.delete(0,"end")
+
 Login_button= tkinter.Button(
     text="Login",
     width=10,
     height=2,
     bg="#61ff96",
     fg="black",
+    command = login
 )
 direc="images\matrix.gif"
 Login_button.pack()
@@ -50,5 +70,31 @@ def update(ind):
 label = tkinter.Label(win)
 label.pack()
 win.after(0, update, 0)
+
+conn = sqlite3.connect('pass_manager.db')
+
+c = conn.cursor()
+
+c.execute("""CREATE TABLE IF NOT EXISTS user_data(
+        username TEXT(20),
+        master_pwd TEXT(20),
+        PRIMARY KEY(username)
+    )""")
+
+c.execute("""CREATE TABLE IF NOT EXISTS user_data_storage(
+        serial_no INTEGER,
+        username TEXT(20),
+        url TEXT,
+        user_name TEXT,
+        service_pwd TEXT,
+        website_name TEXT,
+        PRIMARY KEY(serial_no)
+        FOREIGN KEY(username)
+            REFERENCES user_data(username)
+    )""")
+
+conn.commit()
+
+conn.close()
 
 win.mainloop()
