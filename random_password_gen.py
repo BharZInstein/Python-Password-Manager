e5a50d2704e8fd5ac6b4279224603a288b00362d
Seyan Singampalii <84675733+seyan-s@users.noreply.github.com>
2022-01-18 13:00:22 +0530

    new random password generator

---
+++ random_password_gen.py
@@ -0,0 +1,32 @@
+import random
+import string
+
+length = 6
+
+small = string.ascii_lowercase 
+big = string.ascii_uppercase
+dig = string.digits
+symbols = string.punctuation
+
+TPass1 = random.sample(small,1)
+TPass2 = random.sample(big,1)
+TPass3 = random.sample(dig,1) 
+TPass4 = random.sample(symbols,1)
+
+LPass = TPass1 + TPass2 + TPass3 + TPass4
+
+All = small + big + dig + symbols   
+
+BPass = random.sample(All,length)
+
+Pass = LPass + BPass
+random.shuffle(Pass)
+
+Password = "" .join(Pass)
+
+print(Password)
+
+
+
+
+
