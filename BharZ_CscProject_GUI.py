d3591f683b3d5183b382865c735089fcf93afe0d
Adhiraj Singh <75022578+adhirajsingh1806@users.noreply.github.com>
2022-01-21 18:37:52 +0530

    Update BharZ_CscProject_GUI.py

--- BharZ_CscProject_GUI.py
+++ BharZ_CscProject_GUI.py
@@ -17,13 +17,13 @@ if __name__ == "__main__":
         c = conn.cursor()
         
         if not username:
-            messagebox.error("ERROR","Please enter a valid username in the username box.")
+            messagebox.showerror("ERROR","Please enter a valid username in the username box.")
             return
         else:
             pass
         
         if not password:
-            messagebox.error("ERROR","Please enter a valid password in the password box.")
+            messagebox.showerror("ERROR","Please enter a valid password in the password box.")
             return
         else:
             pass
